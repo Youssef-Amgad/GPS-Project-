3105c82b261ed641cf09dd68b352545a5ea78d4d
Jumana <102044458+Jumy12@users.noreply.github.com>
2023-05-11 03:48:59 +0300

    Update lcd.h

--- Final Delivery/lcd.h
+++ Final Delivery/lcd.h
@@ -14,3 +14,18 @@
 #define CURSOR_ON            0x0E					 
 #define EN									 0x04					 
 #define RS									 0x01					 
+void lcd_init(void);
+void lcd_cmd(unsigned char cmd);
+void lcd_write(unsigned char c, unsigned char ctrl);
+void lcd_char(unsigned char data, unsigned char ctrl);
+void lcd_str( char *str);
+void delayMs(int n);
+void delayUs(int n);
+void reverse(char* str, int len);
+int intToStr(int x, char str[], int d);
+void lcd_str_2nd_row(char *str);
+
+
+
+
+#endif
